4ceee1a88312d0680ed45d1caf8b222cb78f3d32
Jacob Hixon <jacobhixon47@gmail.com>
2018-11-25 23:04:21 -0600

    commit before adding gatsby-plugin-page-transitions

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,25 +10,34 @@ const IndexPage = () => (
       height: '100vh',
       margin: '0',
       backgroundColor: '#ddd',
-      padding: '50px',
-      zIndex: '1',
       backgroundImage: 'url(https://bit.ly/2TEnL1A)'
     }}>
-      <h1 style={{
-        fontSize: '100px',
-        backgroundColor: '#bbb',
-        textAlign: 'left',
-        paddingLeft: '20px',
-        paddingRight: '20px',
-        paddingBottom: '10px',
-        paddingTop: '10px',
-        width: '40vw'
+      <div style={{
+        height: '100vh',
+        width: '100vw',
+        margin: '0',
+        top: '0',
+        left: '0',
+        background: 'rgba(0, 0, 0, 0.4)',
+        zIndex: '1',
+        padding: '50px',
       }}>
-        Hi there!
-      </h1>
-      <h3>Welcome to your new Gatsby site.</h3>
-      <p>Now go build something great.</p>
-      <Link to="/page-2/">Go to page 2</Link>
+        <h1 style={{
+          fontSize: '100px',
+          backgroundColor: '#bbb',
+          textAlign: 'left',
+          paddingLeft: '20px',
+          paddingRight: '20px',
+          paddingBottom: '10px',
+          paddingTop: '10px',
+          width: '40vw'
+        }}>
+          Hi there!
+        </h1>
+        <h3>Welcome to your new Gatsby site.</h3>
+        <p>Now go build something great.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
     </div>
   </Layout>
 )
