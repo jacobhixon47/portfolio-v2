6b8fd472602f7afabe02dab438b6d19e46db57d6
Jacob Hixon <jacobhixon47@gmail.com>
2018-11-29 21:53:53 -0600

    update README

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import {MdMailOutline, MdPhone } from 'react-icons/md';
+import { MdMailOutline } from 'react-icons/md';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import './contact.css';
 
 const Contact = (props) => (
   <div className='contact-section'>
-    <div className='contact-panel no-link'>
-      <h3 style={{ verticalAlign: 'middle'}}>(615) 812-8684<MdPhone style={{marginLeft: '10px'}}/></h3>
-    </div>
     <div className='contact-panel no-link'>
       <h3>jacobhixon47@gmail.com<MdMailOutline style={{marginLeft: '10px'}}/></h3>
     </div>
